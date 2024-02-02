9c39bf0077b1c66a8e35601fa9451806f5e92215
Gayathri Gireesh Sujatha <gayathri.sujatha@arup.com>
2024-02-02 11:22:43 +0530

    Add project files.

---
+++ VSCalculatorCs/Program.cs
@@ -0,0 +1,113 @@
+﻿class Calculator
+{
+    public static double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; 
+
+        
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+   
+        // test
+    
+        Console.WriteLine("gaya's attempt to learn visual studio ;-;");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.Write("Hello, i'm Gaya, what may I call you? ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hellooo {name}, here are instructions for the mathematically challenged :p \nEnter your first number.\nEnter your second numberrr.\nChoose your operation tadaaa!\n");
+
+
+
+        while (!endApp)
+        {
+
+            string numInput1 = "";
+            string numInput2 = "";
+            double result = 0;
+
+            Console.Write("First number please: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("This is not validdd. Enter an integer value pleaseee :'( ");
+                numInput1 = Console.ReadLine();
+            }
+
+            // Ask the user to type the second number.
+            Console.Write("Second number please: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2 = 0;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.Write("This is not validdd. Enter an integer value pleaseee :'( ");
+                numInput2 = Console.ReadLine();
+            }
+
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operation from the following:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Enter please? ");
+
+            string op = Console.ReadLine();
+
+            try
+            {
+                result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred.\n - Details: " + e.Message);
+            }
+
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+            
+            //ok
+            Console.Write($"Ok byeee {name}");
+
+
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n"); // Friendly linespacing.
+        }
+        return;
+    }
+}
